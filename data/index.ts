07999333c5fa5b2af2e827e7060a5855f3019902
sabuhi.isazade@yahoo.com <sabuhi.isazade@yahoo.com>
2024-07-25 11:20:49 +0200

    adding data

---
+++ data/index.ts
@@ -0,0 +1,128 @@
+export const contactElements = [
+  { name: "X", link: "/" },
+  { name: "instagram", link: "/" },
+  { name: "github", link: "/" },
+  { name: "linkedin", link: "/" },
+];
+
+export const carouselElements = [
+  {
+    link: "https://plus.unsplash.com/premium_photo-1678565869434-c81195861939?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    link: "https://images.unsplash.com/photo-1599837565318-67429bde7162?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    link: "https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    link: "https://images.unsplash.com/photo-1579412690850-bd41cd0af397?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    link: "https://images.unsplash.com/photo-1472437774355-71ab6752b434?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+export const Article = [
+  {
+    title: "Understanding Asynchronous JavaScript",
+    date: "July 24, 2023",
+    description:
+      "Dive deep into the world of asynchronous JavaScript, learn about Promises, async/await, and how to handle asynchronous operations efficiently.",
+    link: "/articles/understanding-asynchronous-javascript",
+  },
+  {
+    title: "Mastering React Hooks",
+    date: "June 15, 2023",
+    description:
+      "A comprehensive guide to mastering React Hooks, including useState, useEffect, and custom hooks to enhance your React applications.",
+    link: "/articles/mastering-react-hooks",
+  },
+  {
+    title: "Building Scalable Microservices with Node.js",
+    date: "May 10, 2023",
+    description:
+      "Learn the best practices for building scalable and maintainable microservices using Node.js, Express, and Docker.",
+    link: "/articles/building-scalable-microservices-with-nodejs",
+  },
+  {
+    title: "A Guide to Modern CSS Layouts",
+    date: "April 20, 2023",
+    description:
+      "Explore modern CSS layout techniques, including Flexbox, Grid, and how to create responsive and adaptive web designs.",
+    link: "/articles/guide-to-modern-css-layouts",
+  },
+  {
+    title: "Effective Testing Strategies for JavaScript Applications",
+    date: "March 15, 2023",
+    description:
+      "Discover effective testing strategies for JavaScript applications, including unit testing, integration testing, and end-to-end testing with popular frameworks.",
+    link: "/articles/effective-testing-strategies-for-javascript",
+  },
+  {
+    title: "Optimizing Web Performance",
+    date: "February 5, 2023",
+    description:
+      "Learn how to optimize web performance by understanding critical rendering paths, lazy loading, and best practices for a faster web experience.",
+    link: "/articles/optimizing-web-performance",
+  },
+  {
+    title: "Introduction to TypeScript",
+    date: "January 25, 2023",
+    description:
+      "An introduction to TypeScript, its key features, and how it can help you write more robust and maintainable JavaScript code.",
+    link: "/articles/introduction-to-typescript",
+  },
+];
+
+export const Works = [
+  {
+    name: "Tech Innovators Inc.",
+    position: "Lead Software Engineer",
+    starting_date: "2021",
+    finishing_date: "Present",
+    image:
+      "https://www.reshot.com/preview-assets/icons/HQM63JB2CS/rss-HQM63JB2CS.svg",
+  },
+  {
+    name: "CodeCraft Solutions",
+    position: "Senior Frontend Developer",
+    starting_date: "2018",
+    finishing_date: "2021",
+    image:
+      "https://www.reshot.com/preview-assets/icons/URZ9KT2EDG/snapchat-URZ9KT2EDG.svg",
+  },
+  {
+    name: "NextGen Technologies",
+    position: "Backend Developer",
+    starting_date: "2016",
+    finishing_date: "2018",
+    image:
+      "https://www.reshot.com/preview-assets/icons/UKWGS8QJBT/pinterest-UKWGS8QJBT.svg",
+  },
+  {
+    name: "Innovatech Systems",
+    position: "Junior Developer",
+    starting_date: "2014",
+    finishing_date: "2016",
+    image:
+      "https://www.reshot.com/preview-assets/icons/PTXY7M2H6V/flickr-PTXY7M2H6V.svg",
+  },
+  {
+    name: "FutureSoft Corp.",
+    position: "Intern",
+    starting_date: "2013",
+    finishing_date: "2014",
+    image:
+      "https://www.reshot.com/preview-assets/icons/NK27FXELZD/blackberry-NK27FXELZD.svg",
+  },
+];
+
+
+export const headerElements = [
+  { name: "About", link: "/about" },
+  { name: "Articles", link: "/articles" },
+  { name: "Projects", link: "/projects" },
+  { name: "Speaking", link: "/speaking" },
+  { name: "Uses", link: "/uses" },
+];
